f5af161b275f28f63928d0f899f5f9e59852ac39
MorelCorpJeff <jfmorel@gmail.com>
2023-07-17 20:50:06 +0000

    new web server

--- comments.js
+++ comments.js
@@ -1,36 +1,27 @@
-// create web server
-// 1. load modules
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-// 2. create web server
-var server = http.createServer(function(request,response){
-    var parsedUrl = url.parse(request.url);
-    var res = parsedUrl.pathname;
-    var query = qs.parse(parsedUrl.query);
-    if(res == '/'){
-        fs.readFile('comment.html',function(error,data){
-            response.writeHead(200,{'Content-Type':'text/html'});
-            response.end(data);
-        });
-    } else if(res == '/comment'){
-        var comment = query.comment;
-        fs.appendFile('comment.txt',comment+'\n','utf8',function(error){
-            response.writeHead(200,{'Content-Type':'text/html'});
-            response.end('<h1>comment success</h1>');
-        });
-    } else if(res == '/list'){
-        fs.readFile('comment.txt','utf8',function(error,data){
-            response.writeHead(200,{'Content-Type':'text/html'});
-            response.end(data);
-        });
-    } else {
-        response.writeHead(404,{'Content-Type':'text/html'});
-        response.end('<h1>404 Page Not Found</h1>');
-    }
-});
-// 3. execute web server
-server.listen(80,function(){
-    console.log('Server is running...');
-});
+// create web server with express
+const express = require('express');
+const app = express();
+
+// create a router to handle routes
+const router = express.Router();
+
+// import our controller
+const commentsController = require('../controllers/commentsController');
+
+// handle GET request at /comments
+router.get('/', commentsController.comments);
+
+// handle POST request at /comments
+router.post('/', commentsController.postComment);
+
+// handle GET request at /comments/:commentId
+router.get('/:commentId', commentsController.comment);
+
+// handle PUT request at /comments/:commentId
+router.put('/:commentId', commentsController.putComment);
+
+// handle DELETE request at /comments/:commentId
+router.delete('/:commentId', commentsController.deleteComment);
+
+// export router
+module.exports = router;
